13a058d7c7eed93498c46ecdf1083a3cea34d2ad
kinzahussain-tech <kinza.codingal@gmail.com>
2025-04-10 22:58:13 +0500

    lesson-100 all activities

---
+++ lesson-100-activity-1.py
@@ -0,0 +1,14 @@
+# Storing Values
+a = 98
+b = 94
+c = 41
+d = 95
+e = 11
+
+# Finding the total of trees
+sum = a+b+c+d+e
+print("the sum of all the 5 trees is: ", sum)
+
+# Finding the average of trees
+average = sum/5
+print("the average of all the tree is :", average)
