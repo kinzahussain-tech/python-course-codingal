aa1081bd02c783c9c0b9fc3da8830e2650ed978e
kinzahussain-tech <kinza.codingal@gmail.com>
2025-04-10 18:53:39 +0500

    lesson-98 all activities

---
+++ lesson-98-activity-2.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x = 5
+y = "Kinza"
+print(x)
+print(y)
+
+minion = "codingal"
+print(minion)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to ", minion)
