7365c7ec68034028234642abcbb88d85c8ebffe8
kinzahussain-tech <kinza.codingal@gmail.com>
2025-04-10 22:35:06 +0500

    lesson-99 All activities

---
+++ lesson-99-activity-1.py
@@ -0,0 +1,12 @@
+#check the datatype of different values
+x = 7
+print("type of x: ", type(x))
+
+y=5.5
+print("type of y: ", type(y))
+
+i= "codingally"
+print("type of i: ", type(i))
+
+j= True
+print("type of j: ", type(j))
